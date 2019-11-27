2c29b495ca5182456e6c83898b4f0700637fd5e5
Morie Keita <57179507+Morie55@users.noreply.github.com>
2019-11-27 06:55:36 -0800

    A c++ program to add two numbers

---
+++ addTwoNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int num1,num2,sum;
+cout<<"Input number 1"<<endl;
+cin>>num1;
+cout<<"Input number 2"<<endl;
+cin>>num2;
+sum = num1 + num2;
+cout<<"The sum of the two numbers are="<<sum;
+}
